128db04fd93bcf92b28f18a903c910863eb77c8f
Jocy15 <jociben15@gmail.com>
2019-08-01 16:37:50 -0500

    Initial commit

---
+++ Togehter 48/StoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StoryCollectionViewCell.swift
+//  Togehter 48
+//
+//  Created by Central States SER 02 on 8/1/19.
+//  Copyright © 2019 SER-YCL8. All rights reserved.
+//
+
+import UIKit
+
+class StoryCollectionViewCell: UICollectionViewCell {
+    
+}
